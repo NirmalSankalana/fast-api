fa603e2fe45e5463507fdfa5a72fe7eb2ab1ed79
NirmalSankalana <sankalana.nirmal@gmail.com>
2023-10-03 15:41:24 +0530

    Update Post Table and add authentication when deleting a post

--- app/routes/post_routes.py
+++ app/routes/post_routes.py
@@ -14,7 +14,7 @@ router = APIRouter(
 
 
 @router.get("/", response_model=List[post_schema.Post])
-def get_posts(db: Session = Depends(get_db), user_id:int=Depends(get_current_user)):
+def get_posts(db: Session = Depends(get_db), current_user:int=Depends(get_current_user)):
     posts = db.query(models.Post).all()
     return posts
 
@@ -32,10 +32,10 @@ def get_latest_post(db: Session = Depends(get_db)):
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=post_schema.Post)
-def create_posts(post: post_schema.PostCreate, db: Session = Depends(get_db), user_id:int=Depends(get_current_user)):
+def create_posts(post: post_schema.PostCreate, db: Session = Depends(get_db), current_user:int=Depends(get_current_user)):
     # ** - unpacked the dictionary to the base class
-    print(user_id)
-    new_post = models.Post(**post.dict())
+    print(current_user.id)
+    new_post = models.Post(owner_id = current_user.id, **post.dict())
     db.add(new_post)
     db.commit()
     db.refresh(new_post)
@@ -43,20 +43,24 @@ def create_posts(post: post_schema.PostCreate, db: Session = Depends(get_db), us
 
 
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id: int, db: Session = Depends(get_db), user_id:int=Depends(get_current_user)):
-    post = db.query(models.Post).filter(models.Post.id == id)
-
-    print(f"Succesfully deleted post {id}")
-    if post.first() == None:
+def delete_post(id: int, db: Session = Depends(get_db), current_user:int=Depends(get_current_user)):
+    post_query = db.query(models.Post).filter(models.Post.id == id)
+    post = post_query.first()
+    print(post.owner_id, current_user.id)
+    if post == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"post with {id} does not exists.")
-    post.delete(synchronize_session=False)
+    if int(post.owner_id) != int(current_user.id):
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN)
+    
+    post_query.delete(synchronize_session=False)
     db.commit()
+    print(f"Succesfully deleted post {id}")
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
 @router.put("/{id}", response_model=post_schema.Post)
-def update_post(id: int, post: post_schema.PostCreate, db: Session = Depends(get_db), user_id:int=Depends(get_current_user)):
+def update_post(id: int, post: post_schema.PostCreate, db: Session = Depends(get_db), current_user:int=Depends(get_current_user)):
     post_query = db.query(models.Post).filter(models.Post.id == id)
     post_one = post_query.first()
     if post_one == None:
--- app/schemas/post_schema.py
+++ app/schemas/post_schema.py
@@ -15,6 +15,7 @@ class PostCreate(PostBase):
 class Post(PostBase):
     id: int
     created_at: datetime
+    owner_id: int
 
     class Config:
         orm_mode = True
