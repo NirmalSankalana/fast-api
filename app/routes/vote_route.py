a282ec738f66ad751e0d97d23b527f9fe1778262
Nirmal Sankalana <sankalana.nirmal@gmail.com>
2023-12-22 01:08:29 +0530

    fix does not exists post vote bug

--- app/routes/vote_route.py
+++ app/routes/vote_route.py
@@ -12,13 +12,18 @@ router = APIRouter(
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
 def vote(vote: vote_schema.Vote, db: Session = Depends(database.get_db), current_user: int = Depends(oauth2.get_current_user)):
+    post = db.query(models.Post).filter(models.Post.id == vote.post_id).first()
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Post with id: {vote.post_id} does not exist.")
+
     vote_query = db.query(models.Vote).filter(models.Vote.post_id ==
                                               vote.post_id, models.Vote.user_id == current_user.id)
     found_vote = vote_query.first()
     if vote.dir == 1:
         if found_vote:
-            raise HTTPException(
-                status_code=status.HTTP_409_CONFLICT, detail=f"user has already voted on post {found_vote.post_id}")
+            raise HTTPException(status_code=status.HTTP_409_CONFLICT,
+                                detail=f"user has already voted on post {found_vote.post_id}")
         new_vote = models.Vote(post_id=vote.post_id, user_id=current_user.id)
         db.add(new_vote)
         db.commit()
