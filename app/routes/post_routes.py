3d863fb4756bdbc6306cedf6c19fc8aa6243cc25
Nirmal Sankalana <sankalana.nirmal@gmail.com>
2024-01-02 00:01:08 +0530

    add joins and fixing bugs

--- app/routes/post_routes.py
+++ app/routes/post_routes.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter, Depends, status, HTTPException, Response
+from fastapi.responses import JSONResponse
 from typing import List, Optional
 from sqlalchemy.orm import Session
+from sqlalchemy import func
 
 from ..schemas import post_schema
 from .. import models
@@ -13,14 +15,34 @@ router = APIRouter(
 )
 
 
-@router.get("/", response_model=List[post_schema.Post])
-def get_posts(db: Session = Depends(get_db), current_user:int=Depends(get_current_user), limit:int = 10, skip:int = 0, search: Optional[str]=""):
-    posts = db.query(models.Post).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
-    return posts
+# @router.get("/", response_model=List[post_schema.Post])
+@router.get("/")
+def get_posts(db: Session = Depends(get_db), current_user: int = Depends(get_current_user), limit: int = 10, skip: int = 0, search: Optional[str] = ""):
+    posts = db.query(models.Post).filter(
+        models.Post.title.contains(search)).limit(limit).offset(skip).all()
+
+    results = db.query(models.Post, func.count(models.Vote.post_id).label("votes")).join(
+        models.Vote, models.Vote.post_id == models.Post.id, isouter=True).group_by(models.Post.id).all()
+
+    # Convert the data to a format that can be easily serialized to JSON
+    serialized_results = []
+    for post, vote_count in results:
+        serialized_result = {
+            "post": {
+                "id": post.id,
+                "title": post.title,
+                "content": post.content,
+                # Add other fields as needed
+            },
+            "votes": vote_count
+        }
+        serialized_results.append(serialized_result)
+
+    return JSONResponse(content=serialized_results)
 
 
 @router.get("/{id}", response_model=post_schema.Post)
-def get_post(id: int, db: Session = Depends(get_db), current_user:int=Depends(get_current_user)):
+def get_post(id: int, db: Session = Depends(get_db), current_user: int = Depends(get_current_user)):
     post = db.query(models.Post).filter(models.Post.id == id).first()
     if post == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
@@ -35,10 +57,10 @@ def get_latest_post(db: Session = Depends(get_db)):
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=post_schema.Post)
-def create_posts(post: post_schema.PostCreate, db: Session = Depends(get_db), current_user:int=Depends(get_current_user)):
+def create_posts(post: post_schema.PostCreate, db: Session = Depends(get_db), current_user: int = Depends(get_current_user)):
     # ** - unpacked the dictionary to the base class
     print(current_user.id)
-    new_post = models.Post(owner_id = current_user.id, **post.dict())
+    new_post = models.Post(owner_id=current_user.id, **post.dict())
     db.add(new_post)
     db.commit()
     db.refresh(new_post)
@@ -46,7 +68,7 @@ def create_posts(post: post_schema.PostCreate, db: Session = Depends(get_db), cu
 
 
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id: int, db: Session = Depends(get_db), current_user:int=Depends(get_current_user)):
+def delete_post(id: int, db: Session = Depends(get_db), current_user: int = Depends(get_current_user)):
     post_query = db.query(models.Post).filter(models.Post.id == id)
     post = post_query.first()
     print(post.owner_id, current_user.id)
@@ -55,7 +77,7 @@ def delete_post(id: int, db: Session = Depends(get_db), current_user:int=Depends
                             detail=f"post with {id} does not exists.")
     if int(post.owner_id) != int(current_user.id):
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN)
-    
+
     post_query.delete(synchronize_session=False)
     db.commit()
     print(f"Succesfully deleted post {id}")
@@ -63,7 +85,7 @@ def delete_post(id: int, db: Session = Depends(get_db), current_user:int=Depends
 
 
 @router.put("/{id}", response_model=post_schema.Post)
-def update_post(id: int, post: post_schema.PostCreate, db: Session = Depends(get_db), current_user:int=Depends(get_current_user)):
+def update_post(id: int, post: post_schema.PostCreate, db: Session = Depends(get_db), current_user: int = Depends(get_current_user)):
     post_query = db.query(models.Post).filter(models.Post.id == id)
     post_one = post_query.first()
     if post_one == None:
@@ -71,7 +93,7 @@ def update_post(id: int, post: post_schema.PostCreate, db: Session = Depends(get
                             detail=f"post with {id} does not exists.")
     if int(post_one.owner_id) != int(current_user.id):
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN)
-    
+
     post_query.update(post.dict(), synchronize_session=False)
     db.commit()
     return post_query.first()
---
+++ data.js
@@ -0,0 +1,14 @@
+USER1 = {
+  email: "nipunthejan@gmail.com",
+  password: "azOLe@123",
+};
+
+USER2 = {
+  email: "surani@gmail.com",
+  password: "GoodChild@123",
+};
+
+USER2 = {
+  email: "nirmals.dev@gmail.com",
+  password: "azOLe@123",
+};
